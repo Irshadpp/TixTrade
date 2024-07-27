3a462162c5e88e5d6fcbe70f03a9859e2ef1999d
irshad <irshad89434@gmail.com>
2024-07-27 20:27:19 +0530

    Feat: signup done, jwt left

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,9 @@
 import express, {Request, Response} from "express";
 import { body, validationResult } from "express-validator";
 import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
-
+import { User } from "../model/user";
+import { BadRequestError } from "../errors/bad-request-error";
+import { Password } from "../services/password";
 const router = express();
 
 router.post('/api/users/signup',
@@ -15,16 +16,23 @@ router.post('/api/users/signup',
         .isLength({min:4, max:10})
         .withMessage("Password must be between 4 and 10 charecters")
     ],
-    (req:Request, res:Response)=>{
+    async (req:Request, res:Response)=>{
         const errors = validationResult(req);
         if(!errors.isEmpty()){
             // return res.status(400).send(errors.array());
             throw new RequestValidationError(errors.array())
         }
         const {email, password} = req.body;
-        console.log("creating user");
-        throw new DatabaseConnectionError();
-        res.send({});
+        const existingUser = await User.findOne({email:email});
+        if(existingUser){
+            // console.log("User already exist in ",email);
+            // return res.send({});
+            throw new BadRequestError("Email already in use")
+        }
+        const hashedPassword = await Password.toHash(password)
+        const user = User.build({email, password: hashedPassword});
+        await user.save();
+        res.status(201).send(user);
 });
 
 export {router as signupRouter};
