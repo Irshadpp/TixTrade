4fc86f21e782a828cebb64eb4af26147ceb74a1a
irshad <irshad89434@gmail.com>
2024-08-04 18:55:00 +0530

    listened for events and strip integration left

---
+++ payments/src/index.ts
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+import { app } from "./app";
+import { natsWrapper } from "./nats-wrapper";
+import { OrderCreatedListener } from "./events/listeners/order-created-listener";
+import { OrderCancelledListener } from "./events/listeners/order-cancelled-listener";
+
+const start = async () => {
+  const jwtKey = process.env.JWT_KEY;
+  const MONGO_URI = process.env.MONGO_URI;
+  const NATS_CLUSTER_ID = process.env.NATS_CLUSTER_ID;
+  const NATS_CLIENT_ID = process.env.NATS_CLIENT_ID;
+  const NATS_URL = process.env.NATS_URL;
+
+  if (!jwtKey) {
+    throw new Error("JWT_KEY must be defined");
+  }
+  if (!MONGO_URI) {
+    throw new Error("MONGO_URI must be defined");
+  }
+  if (!NATS_CLUSTER_ID) {
+    throw new Error("NATS_CLUSTER_ID must be defined");
+  }
+  if (!NATS_CLIENT_ID) {
+    throw new Error("NATS_CLIENT_ID must be defined");
+  }
+  if (!NATS_URL) {
+    throw new Error("NATS_URL must be defined");
+  }
+  try {
+    await natsWrapper.connect(NATS_CLUSTER_ID, NATS_CLIENT_ID, NATS_URL);
+
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed!");
+      process.exit();
+    });
+
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
+
+    new OrderCreatedListener(natsWrapper.client).listen();
+    new OrderCancelledListener(natsWrapper.client).listen();
+
+    await mongoose.connect(MONGO_URI);
+    console.log("payments-mongodb connected");
+  } catch (error) {
+    console.log(error);
+  }
+  app.listen(3000, () => {
+    console.log("payments Listening on port 3000!!");
+  });
+};
+start();
---
+++ payments/src/test/setup.ts
@@ -0,0 +1,54 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import { app } from "../app";
+import  request  from "supertest";
+import jwt from 'jsonwebtoken'
+
+jest.mock('../nats-wrapper.ts');
+
+let mongo: any;
+beforeAll(async () => {
+
+  process.env.JWT_KEY = "abcde"
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+
+  await mongoose.connect(mongoUri);
+});
+
+beforeEach(async () => {
+  jest.clearAllMocks();
+  const collections = await mongoose.connection.db.collections();
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongoose.disconnect();
+  await mongo.stop();
+});
+
+
+export const signin = (id?: string): string[] =>{
+  //Build a jwt payload {id,email}
+  const payload = {
+    id: id || new mongoose.Types.ObjectId().toHexString(),
+    email: "test@gmail.com",
+  }
+  //create the jwt
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+  //build session object {jwt: MY_JWT}
+  const session = {jwt: token}
+
+  //turn the session into JSON
+  const sessionJSON = JSON.stringify(session);
+
+  //encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString('base64');
+
+  //return a string thats the cookie with the encoded data
+  return [`session=${base64}`];
+
+}
